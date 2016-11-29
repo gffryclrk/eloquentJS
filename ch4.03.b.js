b8f617c2fa880642ce8034569ad1af691bbbba23
clark <clark@clarks-MacBook-Pro.local>
2016-11-29 08:24:00 -0500

    Finished listToArray function

--- ch4.03.b.js
+++ ch4.03.b.js
@@ -1,14 +1,36 @@
 var arrayToList = function(array){
 	var list = {};
 	var rest = null;
-	for(var i = 0; i < array.length; i++){
+	for(var i = array.length; i >= 0; i--){
 		list = { value: array[i], rest: rest};
 		rest = list;
 	}
 	return list;
 }
-
+/*
 var listToArray = function(list, array){
 	if(array == undefined) var array = [];
-	array.push(list.value);
+	if(list.rest == null){
+		return array;
+	}else{
+		array.push(list.value);
+		listToArray(list.rest, array);
+	}
+} */
+var listToArray = function(list){
+	var array = [];
+	while(list.rest !== null){
+		array.push(list.value);
+		list = list.rest;
+	}
+	return array;
 }
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+//console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+//console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
