e8128738b626b58c6daefca564497f2835b23ecb
gffryclrk <mr@gffryclrk.com>
2017-01-18 05:51:59 -0500

    Finished Ch5: Mother-child age difference

--- ch5.02.js
+++ ch5.02.js
@@ -1,3 +1,5 @@
+var ancestry = JSON.parse(ANCESTRY_FILE);
+
 function average(array) {
 	function plus(a, b) { return a + b; }
 	return array.reduce(plus) / array.length;
@@ -7,3 +9,17 @@ var byName = {};
 ancestry.forEach(function(person){
 	byName[person.name] = person;
 });
+
+var motherBirthAge = [];
+ancestry.forEach(function(person){
+	var mother = byName[person.mother];
+	//motherBirthAge.push(mother);
+	if(typeof mother !== 'undefined') {
+		//birthAge = byName[person.born] - mother.born;
+		birthAge = person.born - mother.born;
+		motherBirthAge.push(birthAge);
+		//motherBirthAge.push(person.born - mother.born); 
+	}
+})
+
+averageAgeDifference = average(motherBirthAge);
