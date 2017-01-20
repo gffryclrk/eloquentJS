0c2f3bfaca7dd67f8c9f5eb17d3faf5150051fb3
gffryclrk <mr@gffryclrk.com>
2017-01-20 15:06:07 -0500

    Ch5 Ex3: Finished first working version

--- ch5.03.js
+++ ch5.03.js
@@ -5,9 +5,20 @@ function average(array){
 	return array.reduce(plus) / array.length;
 }
 
+var i = 0;
 var byCentury = {};
 ancestry.forEach(function(person){
 	var century = Math.ceil(person.died / 100); 
-	if(typeof byCentury[century] == 'undefined') var byCentury[century] = [];
+	console.log(typeof century);
+
+	// console.log(century);
+	if(typeof byCentury[century] == "undefined") { byCentury[century] = [];	}
+	// if(typeof byCentury[century] == 'undefined') var byCentury[century] = [];
 	byCentury[century].push(person.died - person.born);
 });
+
+for(var key in byCentury){
+	if(byCentury.hasOwnProperty(key)){
+		console.log(key + " " + average(byCentury[key]));
+	}
+}
