6e3b96c8cab4d1d32d8e76fea2c4a5c076fd14fd
gffryclrk <mr@gffryclrk.com>
2017-01-20 15:18:46 -0500

    Removed some redundant lines

--- ch5.03.js
+++ ch5.03.js
@@ -5,15 +5,11 @@ function average(array){
 	return array.reduce(plus) / array.length;
 }
 
-var i = 0;
 var byCentury = {};
 ancestry.forEach(function(person){
 	var century = Math.ceil(person.died / 100); 
-	console.log(typeof century);
 
-	// console.log(century);
 	if(typeof byCentury[century] == "undefined") { byCentury[century] = [];	}
-	// if(typeof byCentury[century] == 'undefined') var byCentury[century] = [];
 	byCentury[century].push(person.died - person.born);
 });
 
