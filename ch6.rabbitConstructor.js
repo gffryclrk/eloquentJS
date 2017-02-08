89db07372b6d895834774e6d5b9229bfa70d3e0f
gffryclrk <mr@gffryclrk.com>
2017-02-08 13:46:42 -0500

    Chapter 6: Polymorphism

--- ch6.rabbitConstructor.js
+++ ch6.rabbitConstructor.js
@@ -4,3 +4,15 @@ function Rabbit(type){
 
 var killerRabbit = new Rabbit("killer");
 var blackRabbit = new Rabbit("black");
+
+Rabbit.prototype.speak = function (line) {
+	console.log("The " + this.type + " rabbit says '" + line + "'" );
+};
+// blackRabbit.speak("Doom...");
+
+// Prototype Interference
+
+Rabbit.prototype.dance = function() {
+	console.log("The " + this.type + " rabbit dances a jig.");
+};
+killerRabbit.dance();
